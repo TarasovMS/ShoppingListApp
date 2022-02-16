cd917311264fa1951e3aef9719419ac038e4198b
Cruellok <80469992+Cruellok@users.noreply.github.com>
2022-02-17 01:49:37 +0400

    edit 17.02.2022

--- shopping_list/app/src/main/java/com/persAssistant/shopping_list/presentation/fragment/list_of_shopping_list_fragment/ListOfShoppingListFragment.kt
+++ shopping_list/app/src/main/java/com/persAssistant/shopping_list/presentation/fragment/list_of_shopping_list_fragment/ListOfShoppingListFragment.kt
@@ -2,50 +2,44 @@ package com.persAssistant.shopping_list.presentation.fragment.list_of_shopping_l
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.persAssistant.shopping_list.base.BaseFragment
+import androidx.fragment.app.viewModels
+import com.persAssistant.shopping_list.R
+import com.persAssistant.shopping_list.base.AppBaseFragment
 import com.persAssistant.shopping_list.domain.entities.ShoppingList
 import com.persAssistant.shopping_list.databinding.RecyclerShoppingListBinding
 import com.persAssistant.shopping_list.presentation.App
+import com.persAssistant.shopping_list.presentation.activity.shopping_list.CreatorShoppingListActivity
 import com.persAssistant.shopping_list.presentation.fragment.list_of_purchase_fragment.ListOfPurchaseViewModel
 import com.persAssistant.shopping_list.presentation.activity.shopping_list.EditorShoppingListActivity
-import com.persAssistant.shopping_list.presentation.fragment.list_of_shopping_list_fragment.ListOfShoppingListViewModel
-import com.persAssistant.shopping_list.presentation.fragment.list_of_shopping_list_fragment.OnShoppingListClickListener
-import com.persAssistant.shopping_list.presentation.fragment.list_of_shopping_list_fragment.ShoppingListAdapter
-import java.util.*
+import com.persAssistant.shopping_list.presentation.util.viewBinding
 
-class ListOfShoppingListFragment: BaseFragment() {
+class ListOfShoppingListFragment: AppBaseFragment(R.layout.recycler_shopping_list) {
 
-    private lateinit var viewModel: ListOfShoppingListViewModel
-    private lateinit var ui: RecyclerShoppingListBinding
+
+    private val ui: RecyclerShoppingListBinding by viewBinding (RecyclerShoppingListBinding::bind)
+
+    private val viewModel: ListOfShoppingListViewModel by viewModels { viewModelFactory }
 
     private lateinit var shoppingListAdapter: ShoppingListAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        ui = RecyclerShoppingListBinding.inflate(layoutInflater)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val bundle = Bundle()
 
         // initialize Adapter
-        shoppingListAdapter = ShoppingListAdapter(LinkedList(), object:
-            OnShoppingListClickListener {
-            override fun clickedShoppingListItem(shoppingList: ShoppingList) {
-//                val intent = ListOfPurchaseActivity.getIntentByShoppingListId(requireContext(),shoppingList.id!!)
-//                val intent = ListOfPurchaseFragment.getIntentByShoppingListId(requireActivity().intent, )
-//                startActivity(intent)
-//                val KEY_INDEX_TYPE = "INDEX_TYPE"
-//                val KEY_PARENT_ID = "PARENT_ID"
-//                val KEY_VISIBILITY_BUTTON = "VISIBILITY_BUTTON"
+        shoppingListAdapter = ShoppingListAdapter( object: OnShoppingListClickListener {
 
+            override fun clickedShoppingListItem(shoppingList: ShoppingList) {
                 bundle.putLong(KEY_PARENT_ID, shoppingList.id!!)
                 bundle.putBoolean(KEY_VISIBILITY_BUTTON, true)
                 bundle.putInt(KEY_INDEX_TYPE, ListOfPurchaseViewModel.IdTypes.SHOPPINGLIST.ordinal)
 
-//                val fragmentManager = childFragmentManager.beginTransaction()
-//                fragmentManager.replace(R.id.fragment, ListOfPurchaseFragment())
-//                fragmentManager.commit()
+//                val iuRouter = UiRouter(navController = nav)
+                /**
+                 * здесь выполнить клик item
+                 */
 
             }
 
@@ -61,7 +55,7 @@ class ListOfShoppingListFragment: BaseFragment() {
         ui.recyclerViewShoppingList.adapter = shoppingListAdapter
 
         // initialize ViewModel
-        viewModel = app.appComponent.getListOfShoppingListViewModel()
+//        viewModel = app.appComponent.getListOfShoppingListViewModel()
 
         viewModel.deleteShoppingListId.observe(requireActivity(), androidx.lifecycle.Observer {
             shoppingListAdapter.removeShoppingList(it)
@@ -73,12 +67,11 @@ class ListOfShoppingListFragment: BaseFragment() {
 
         viewModel.init(this)
 
-//        ui.btnAddShoppingList.setOnClickListener {
-//            val intent = CreatorShoppingListActivity.getIntent(requireContext())
-//            startActivity(intent)
-//        }
-        
-        return ui.root
+        ui.btnAddShoppingList.setOnClickListener {
+            val intent = CreatorShoppingListActivity.getIntent(requireContext())
+            startActivity(intent)
+        }
+
     }
 
     override fun onAttach(context: Context) {
