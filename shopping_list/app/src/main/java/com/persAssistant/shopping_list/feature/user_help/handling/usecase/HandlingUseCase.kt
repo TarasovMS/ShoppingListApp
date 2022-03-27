3326dad9ea1a4114b39778b3906a7ffee6f52e48
Tarasov <80469992+Cruellok@users.noreply.github.com>
2022-03-28 01:22:21 +0400

    feat: ref code

--- shopping_list/app/src/main/java/com/persAssistant/shopping_list/feature/user_help/handling/usecase/HandlingUseCase.kt
+++ shopping_list/app/src/main/java/com/persAssistant/shopping_list/feature/user_help/handling/usecase/HandlingUseCase.kt
@@ -3,21 +3,20 @@ package com.persAssistant.shopping_list.feature.user_help.handling.usecase
 import com.persAssistant.shopping_list.error.ExecutionResult
 import com.persAssistant.shopping_list.error.Failure
 import com.persAssistant.shopping_list.error.RegistrationError
-import com.persAssistant.shopping_list.util.validateEmail
-import com.persAssistant.shopping_list.util.validateName
+import com.persAssistant.shopping_list.util.validateFields
 import javax.inject.Inject
 
 class HandlingUseCase @Inject constructor() {
 
     fun validateName(name: String): ExecutionResult<Failure, String> {
-        return if (name.validateName())
+        return if (name.validateFields())
             ExecutionResult.Success(name)
         else
             ExecutionResult.Fail(RegistrationError.NameValidationError)
     }
 
     fun validateMessage(message: String): ExecutionResult<Failure, String> {
-        return if (message.validateEmail())
+        return if (message.validateFields())
             ExecutionResult.Success(message)
         else
             ExecutionResult.Fail(RegistrationError.MessageValidationError)
--- shopping_list/app/src/main/java/com/persAssistant/shopping_list/util/ValidateExtensions.kt
+++ shopping_list/app/src/main/java/com/persAssistant/shopping_list/util/ValidateExtensions.kt
@@ -4,7 +4,7 @@ import android.util.Patterns
 
 fun String.validateEmail() = Patterns.EMAIL_ADDRESS.matcher(this).matches()
 
-fun String.validateName() = "\\D".toRegex().matches(this)
+fun String.validateFields() = this.isNotEmpty()
 
 
 
